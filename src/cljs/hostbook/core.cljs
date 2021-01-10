29f9864349da1e53a6f1db881da3409ed0e33f56
cindy <ciindy.wu@gmail.com>
2021-01-09 14:19:34 -1000

    Add errors component

--- src/cljs/hostbook/core.cljs
+++ src/cljs/hostbook/core.cljs
@@ -1,31 +1,42 @@
 (ns hostbook.core
   (:require 
    [reagent.core :as reagent :refer [atom]]
-   [ajax.core :refer [GET POST]]))
+   [ajax.core :refer [POST]]))
 
-(defn send-message! [fields]
+(defn errors-component [errors id]
+  (when-let [error (id @errors)]
+    [:div.alert.alert-danger (clojure.string/join error)]))
+
+(defn send-message! [fields errors]
   (POST "/add-message"
     {:format :json
      :headers
      {"Accept" "application/transit+json"
       "x-csrf-token" (.-value (.getElementById js/document "token"))}
      :params @fields
-     :handler #(.log js/console (str "response:" %))
-     :error-handler #(.error js/console (str "error:" %))}))
+     :handler #(do
+                 (.log js/console (str "response:" %))
+                 (reset! errors nil))
+     :error-handler #(do
+                       (.log js/console (str %))
+                       (reset! errors (get-in % [:response :errors])))}))
 
 (defn message-form []
-  (let [fields (atom{})]
+  (let [fields (atom {})
+        errors (atom nil)]
     (fn []
       [:div.content
        [:div.form-group
         [:p "name:" (:name @fields)]
         [:p "message:" (:message @fields)]
+        [errors-component errors :name]
         [:p "Name:"
          [:input.form-control
           {:type :text
            :name :name
            :on-change #(swap! fields assoc :name (-> % .-target .-value))
            :value (:name @fields)}]]]
+       [errors-component errors :message]
        [:p "Message:"
         [:textarea.form-control
          {:rows 4
@@ -35,7 +46,7 @@
          (:message @fields)]]
        [:input.btn.btn-primary 
         {:type :submit 
-         :on-click #(send-message! fields)
+         :on-click #(send-message! fields errors)
          :value "comment"}]])))
 
 (defn home []
